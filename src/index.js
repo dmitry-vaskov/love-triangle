2b1f3a02d461cf8df924f4a6af22f0e43d4afb3f
dmitry-vaskov <47758796+dmitry-vaskov@users.noreply.github.com>
2019-03-02 13:16:08 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -4,4 +4,10 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var triangles = 0;
+  for (var i = 0; i < preferences.length; i++) {
+    if (preferences[preferences[preferences[i] - 1] - 1] == i + 1)
+      triangles++;
+  }
+  return parseInt(triangles / 3);
 };
